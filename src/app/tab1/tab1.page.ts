7c1278d1ab8a780571e7e5c48e5243c7f24ca8ba
Miguel Jose Albano da Silva <61022078+josemiguel2004@users.noreply.github.com>
2021-08-12 12:20:46 -0300

    12/08/2021
    Adicionei Alert e Toast.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,39 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+
+  async exibirAlerta() {
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: 'Você quer visualizar este pokemon?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Sim, confirmo',
+          handler: () => {
+            this.apresentarToast();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'Confirmação realizada com sucesso!',
+      duration: 2000,
+      color: 'verde-claro'
+    });
+    toast.present();
+  }
 
 }
